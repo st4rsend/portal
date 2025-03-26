08e473b74d47ec54479edc3854312f48b70793dc
psychotictac@gmail.com <psychotictac@gmail.com>
2025-03-26 08:30:18 +0000

    security fix

---
+++ src/app/productions/firestore/katex/katex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-katex',
+  imports: [],
+  templateUrl: './katex.component.html',
+  styleUrl: './katex.component.sass'
+})
+export class KatexComponent {
+
+}
